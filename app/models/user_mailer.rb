b70bf645261deda5f3a39e575e6793612c7edbe5
lnookx <lnookx@googlemail.com>
2009-12-29 17:00:06 +0000

    Added roles and permissions. Added mailings for forgotten passwords. etc.

--- app/models/user_mailer.rb
+++ app/models/user_mailer.rb
@@ -2,15 +2,24 @@ class UserMailer < ActionMailer::Base
   def signup_notification(user)
     setup_email(user)
     @subject    += 'Please activate your new account'
-  
-    @body[:url]  = "http://YOURSITE/activate/#{user.activation_code}"
-  
+    @body[:url]  = "http://localhost:3000/activate/#{user.activation_code}"
   end
   
   def activation(user)
     setup_email(user)
     @subject    += 'Your account has been activated!'
-    @body[:url]  = "http://YOURSITE/"
+    @body[:url]  = "http://localhost:3000/"
+  end
+
+  def forgot_password(user)
+    setup_email(user)
+    @subject += 'You have requested to change your password'
+    @body[:url] = "http://localhost:3000/reset_password/#{user.password_reset_code}"
+  end
+
+  def reset_password(user)
+    setup_email(user)
+    @subject += 'Your password has been reset.'
   end
   
   protected
